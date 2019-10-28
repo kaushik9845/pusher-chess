d2fb17a7a6976caa9ead4510e959c944fc6b524f
kaushik9845 <kaushik9845@gmail.com>
2019-10-28 13:00:20 +0530

    Update chatkit.js

--- chess-backend/routes/chatkit.js
+++ chess-backend/routes/chatkit.js
@@ -1,8 +1,8 @@
 var Chatkit = require('@pusher/chatkit-server');
 
 const chatkit = new Chatkit.default({
-    instanceLocator: 'CHATKIT_INSTANCE_LOCATOR',
-    key: 'CHATKIT_SECRET_KEY'
+    instanceLocator: 'v1:us1:539de3eb-f409-40f3-99f2-ab76098c9504',
+    key: '1360c3eb-6d8e-49fe-8bd4-f2f160fff0a9:lU9dxmOQowcvAlUrikE1DtyVTYTy6qm05Uv/VCqQI4I='
 });
 
 module.exports = chatkit;
